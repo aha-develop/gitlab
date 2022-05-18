c97352ae7d21c008aabf87770424072b56fd3b97
Nathan Wright <nathan@brandalism.com>
2022-05-17 17:10:24 -0700

    Update code to use new aha-menu components

--- src/components/attribute/Menu.tsx
+++ src/components/attribute/Menu.tsx
@@ -9,12 +9,20 @@ export type MenuProps = {
 export const Menu = ({ record }: MenuProps) => {
   return (
     <aha-menu>
-      <aha-button slot="button" type="attribute" size="small">
+      <aha-button slot="control" kind="attribute" size="small">
         <aha-icon icon="fa-solid fa-ellipsis"></aha-icon>
       </aha-button>
-      <aha-menu-item onClick={() => runCommand(record, 'sync')}>Resync</aha-menu-item>
-      <aha-menu-item onClick={() => runCommand(record, 'addLink')}>Link merge request</aha-menu-item>
-      <aha-menu-item onClick={() => runCommand(record, 'removeLinks')}>Unlink merge requests</aha-menu-item>
+      <aha-menu-content>
+        <aha-menu-item>
+          <a href="#" onClick={() => runCommand(record, 'sync')}>Resync</a>
+        </aha-menu-item>
+        <aha-menu-item>
+          <a href="#" onClick={() => runCommand(record, 'addLink')}>Link merge request</a>
+        </aha-menu-item>
+        <aha-menu-item>
+          <a href="#" onClick={() => runCommand(record, 'removeLinks')}>Unlink merge requests</a>
+        </aha-menu-item>
+      </aha-menu-content>
     </aha-menu>
   );
 };
