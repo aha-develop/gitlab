0b7258834310985a99e57c20e1b71dab7ce54921
Jeremy Wells <jemmyw@gmail.com>
2022-08-11 14:39:24 +1200

    Revert changes to getMRFromURL

--- src/lib/gitlab/getMRFromURL.ts
+++ src/lib/gitlab/getMRFromURL.ts
@@ -11,9 +11,6 @@ export const getMRFromURL =
   (url: string) =>
   async (api): Promise<Gitlab.MR> => {
     const { repo, mrId } = parseGitlabURL(url);
-    console.log('preflight');
-    const response = await api.request(GetMRByIid, { fullPath: repo, iids: [mrId] });
-    console.log(response, response);
-    const { project = undefined } = response;
+    const { project = undefined } = await api.request(GetMRByIid, { fullPath: repo, iids: [mrId] });
     return project?.mergeRequests?.nodes?.[0] ?? undefined;
   };
